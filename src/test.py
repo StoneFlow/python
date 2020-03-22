4cba4e86c7fd05fefc0bd13bb6d45e00c90958b3
Your Name <email@example.com>
2020-03-22 23:19:35 +0800

    add zifuzhuanhuan

--- src/test.py
+++ src/test.py
@@ -1,6 +1,29 @@
 # 字符转换
+num1 = 1 
+str1 = '111'
 
-ni = input("Please input")
-print(ni)
-print(type(ni)) 
-print(type(int(ni)))
+print(type(float(num1)))
+print(float(num1))
+
+print(float(str1))
+
+
+print(type(str(num1)))
+
+list1 = [10,20,30]
+print(tuple(list1))
+
+t1 = (100,2020,43242)
+print(list(t1))
+
+str2 = '1'
+print(type(eval(str2)))
+str3 = '1.1'
+print(type(eval(str3)))
+str4 = '(100,200,300)'
+print(type(eval(str4)))
+str5 = '[111,222,333]'
+print(type(eval(str5)))
+
+
+
