50d7a6776a5b7cc4b102dc70d4fcc7866116401b
Your Name <email@example.com>
2020-03-22 20:32:34 +0800

    add shujuleixing

--- src/test.py
+++ src/test.py
@@ -1,11 +1,22 @@
-my_name =  'Tom'
+#数据类型
+num1 = 1
+num2 = 1.1
+print(type(num2))
 
-print(my_name)
+a = 'hello world'
+print(type(a))
 
-schoolName = 'di jia'
-print(schoolName)
+s = True
+print(type(s))
 
+c = [10,20,30]
+print(type(c))
 
-Student = 'wa zi'
-print(Student)
+d = (1,23,45)
+print(type(d))
 
+e = {12,432,23}
+print(type(e))
+
+w = {'name': 'Tom', 'age': 18}
+print(type(w))
