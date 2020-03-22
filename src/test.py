8749faf7d49951ccec910d8b2aab31766c7d796f
Your Name <email@example.com>
2020-03-22 22:20:50 +0800

    add zhuanyi

--- src/test.py
+++ src/test.py
@@ -1,21 +1,4 @@
-# 输出
+#转义字符
 
-age = 18
-name = 'dijia'
-weight = 55.53
-stu_id = 1
-
-# 1.今年我的年龄是x岁
-print('My age is %d this year' %age)
-# 2.我的名字是s
-print('My name is %s' %name)
-# 3.我的体重是a公斤
-print('My weight is %.2f Kg' %weight)
-# 4.我的学号是e
-print('My stu_id is %03d' %stu_id)
-# 5.我的名字是s，今年x岁了
-print('My name is %s,%d years old' %(name, age))
-# 6.我的名字e，明年c岁了，体重a公斤，学号是e
-print('My name is %s,%d years old,weight %.2f kg'%(name, age + 1, weight))
-
-print(f'My name {name}')
+print('hello\nworld')
+print('\tasdfg')
