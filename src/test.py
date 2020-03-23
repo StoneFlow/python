ff9d98f0cbf8c590896e48d7b4648814c6a84f80
Your Name <email@example.com>
2020-03-23 12:07:46 +0800

    add fuzhiyunsuanfu

--- src/test.py
+++ src/test.py
@@ -1,8 +1,29 @@
-# 赋值运算
+# 复合赋值运算符
 
-num1, num2, num3 = 1, 2, 3
-print(num1, num2, num3)
+a = 10
+a += 1
+print(a)
 
-num4 = num5 = 10
-print(num4)
-print(num5)
+b = 10
+b -= 1
+print(b)
+
+c = 10
+c *= 10
+print(c)
+
+d = 10
+d /= 2
+print(d)
+
+e = 10
+e //= 3
+print(e)
+
+f = 10
+f %= 3
+print(f)
+
+g = 10
+g **= 10
+print(g)
