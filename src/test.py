538cf1812b20c20d20738e720e537538d0fbf096
Your Name <email@example.com>
2020-03-23 11:42:26 +0800

    add zifuyunsuan

--- src/test.py
+++ src/test.py
@@ -1,29 +1,10 @@
-# 字符转换
-num1 = 1 
-str1 = '111'
+# 字符运算
 
-print(type(float(num1)))
-print(float(num1))
-
-print(float(str1))
-
-
-print(type(str(num1)))
-
-list1 = [10,20,30]
-print(tuple(list1))
-
-t1 = (100,2020,43242)
-print(list(t1))
-
-str2 = '1'
-print(type(eval(str2)))
-str3 = '1.1'
-print(type(eval(str3)))
-str4 = '(100,200,300)'
-print(type(eval(str4)))
-str5 = '[111,222,333]'
-print(type(eval(str5)))
-
-
+print(10+10)
+print(10-10)
+print(10*10)
+print(10/10)
+print(235//10)
+print(244%10)
+print(3*3**2)
 
