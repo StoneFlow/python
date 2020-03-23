263c9e641ef65560ffebdfcf8baf9acc4ea94274
Your Name <email@example.com>
2020-03-23 14:14:42 +0800

    add luojiyunsuanfu

--- src/test.py
+++ src/test.py
@@ -1,29 +1,10 @@
-# 复合赋值运算符
+# 逻辑运算符号
+a = 0
+b = 1
+c = 2
 
-a = 10
-a += 1
-print(a)
-
-b = 10
-b -= 1
-print(b)
-
-c = 10
-c *= 10
-print(c)
-
-d = 10
-d /= 2
-print(d)
-
-e = 10
-e //= 3
-print(e)
-
-f = 10
-f %= 3
-print(f)
-
-g = 10
-g **= 10
-print(g)
+print(a < b and c > b)
+print(a > b and c > b)
+print(a > b or  c > b)
+print(not a < b)
+print(not False)
